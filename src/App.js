f10b757ab6f951b6e375428af857eec98ab933ec
Navin Kumar <navinkumar0437@gmail.com>
2023-09-17 02:10:29 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomePage from './pages/homepage'
+
+const App = () => {
+  return <HomePage />
+}
+
+export default App
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default HeroSection
