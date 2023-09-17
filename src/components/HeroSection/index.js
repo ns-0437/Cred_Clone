bdba483063dde5a37f4957ec24176975f94a2d6f
Navin Kumar <navinkumar0437@gmail.com>
2023-09-18 01:40:43 +0530

    first page of cred

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Button from "../common/Button";
+import './heroSection.css';
 
 const HeroSection = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="hero-section-wrapper">
+      <div className="flex absolute-center hero-claim-lable">
+        {/* <div>pay credit card bill. earn guranteed cashback ₹200</div> */}
+      </div>
+      <div className="flex absolute-center flex-col hero-section max-width">
+        <div className="hero-heading">
+          reward for paying credit card bills.
+        </div>
+        <div className="hero-subheading">
+          join 9M+ members who win rewards and cashbacks everyday
+        </div>
+        <Button buttonText="Download CRED" />
+      </div>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
