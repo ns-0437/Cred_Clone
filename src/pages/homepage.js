aa860498efd97ae913b31567c0d830961cdd0d64
Navin Kumar <navinkumar0437@gmail.com>
2023-11-28 15:26:11 +0530

    Rating page completed

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,8 +5,11 @@ import ProductShowcase from '../components/ProductShowcase';
 import FeelSpecial from '../components/FeelSpecial';
 import CredExperience from '../components/CredExperience';
 import CredSecurity from '../components/CredSecurity';
+import CredStory from '../components/CredStory';
 import BrandsLove from '../components/BrandsLove';
 import WindowPeak from '../components/WindowPeak';
+import MobileScroll from '../components/MobileScroll';
+import AppRating from '../components/AppRating';
 
 
 const HomePage = () => {
@@ -18,11 +21,13 @@ const HomePage = () => {
       <FeelSpecial />
       <BrandsLove />
       <CredExperience />
+      <MobileScroll />
       <div className='non-mobile'>
         <WindowPeak />
       </div>
       <CredSecurity />
-
+      <CredStory />
+      <AppRating />
     </>
   );
 };
